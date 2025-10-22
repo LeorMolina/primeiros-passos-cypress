519163f6fca47dd11cc8439589055c547d2abe64
Leo Molina <leomolinaruan@gmail.com>
2025-10-21 22:06:08 -0300

    First Commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,7 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  })
+});
+
+it('teste', function() {});
